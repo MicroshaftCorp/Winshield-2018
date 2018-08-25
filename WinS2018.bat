6f9b98888e0cb3450c77f359cd32e22be7c13a51
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-08-25 12:30:34 -0700

    Create WinS2018.bat

---
+++ WinS2018.bat
@@ -0,0 +1,21 @@
+@echo off
+cls
+title Winshield 2018 Offical!
+msg *Welcome to Winzhield Antiviruz!
+set /p a=Enter a batch file to scan:
+echo SYSTE32 #%random% >C:\Windows\%random%%random%.virusinfectioncollect.txt
+echo SYSTE32 #%random% >C:\Windows\%random%%random%.virusinfectioncollect.txt
+echo SYSTE32 #%random% >C:\Windows\%random%%random%.virusinfectioncollect.txt
+echo SYSTE32 #%random% >C:\Windows\%random%%random%.virusinfectioncollect.txt
+echo SYSTE32 #%random% >C:\Windows\%random%%random%.virusinfectioncollect.txt
+echo SYSTE32 #%random% >C:\Windows\%random%%random%.virusinfectioncollect.txt
+echo Checking %a%.bat.....
+echo
+echo
+echo
+echo
+echo Press Any Key for results!
+pause
+echo Virus Was Detected!!
+echo %a%.bat was deleted by Winshield antivirus....
+
